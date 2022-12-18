8414738cf1dad92c7c8e12d9b68dd80614a7b5d7
Ray Nirola <ray@nirola.in>
2022-12-18 08:21:56 +0530

    delay server reponse and random health status

--- packages/server/src/trpc.ts
+++ packages/server/src/trpc.ts
@@ -1,6 +1,8 @@
 import { initTRPC, inferAsyncReturnType } from "@trpc/server";
 import { CreateExpressContextOptions } from "@trpc/server/adapters/express";
 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export const createContext = (opts: CreateExpressContextOptions) => ({
   ...opts,
 });
@@ -10,7 +12,10 @@ type Context = inferAsyncReturnType<typeof createContext>;
 const trpc = initTRPC.context<Context>().create();
 
 const router = trpc.router({
-  health: trpc.procedure.query(() => ({ status: "down" })),
+  health: trpc.procedure.query(async () => {
+    await delay(2000);
+    return { status: Math.random() > 0.5 ? "ok" : "down" };
+  }),
 });
 
 export type Router = typeof router;
